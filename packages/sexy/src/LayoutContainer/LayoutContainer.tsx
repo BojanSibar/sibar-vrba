6decf0eade409d316903553b84004f9ed9304fd9
Bojan Sibar <bojan.sibar@symphony.is>
2020-01-13 08:53:57 +0100

    sexy change

--- packages/sexy/src/LayoutContainer/LayoutContainer.tsx
+++ packages/sexy/src/LayoutContainer/LayoutContainer.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from "react";
 
 const LayoutContainer: FC = () => {
-  return <div>This might works!!!!!!!!!</div>;
+  return <div>This might works!!!!!!!!! And it is sexy</div>;
 };
 
 export default LayoutContainer;
