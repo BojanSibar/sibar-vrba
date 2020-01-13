c02b67404bb5ec471faec0fa3c33f89769bd6ff5
Bojan Sibar <bojan.sibar@symphony.is>
2020-01-13 09:06:40 +0100

    add alert as business required

--- packages/sexy/src/LayoutContainer/LayoutContainer.tsx
+++ packages/sexy/src/LayoutContainer/LayoutContainer.tsx
@@ -1,9 +1,15 @@
 import React, { FC } from "react";
 
 const LayoutContainer: FC = () => {
+  const logMe = () => {
+    alert("idemo");
+  };
   return (
     <div>
       This might works!!!!!!!!! And it is sexy, and we know it.!!! Yeahhh
+      <div>
+        <button onClick={logMe}>Click me</button>
+      </div>
     </div>
   );
 };
